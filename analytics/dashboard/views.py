d1b5f0778921f4fd31028bea4687cc5e4e582e8b
sarathi475 <sarathi475@gmail.com>
2020-12-12 11:27:34 +0530

    ing pandas and csv file tried sorting the values from the table in html page.

--- analytics/dashboard/views.py
+++ analytics/dashboard/views.py
@@ -1,6 +1,59 @@
 from django.shortcuts import render
+import pandas as pd
+from .models import StateWiseTesting
+from django.shortcuts import HttpResponse 
+import json
+
 
 # Create your views here.
 
-def dashboard_home(request):
-    return render(request, "download_home.html")
+
+def dashboard_home(request):    
+
+    df = pd.read_csv('/home/chadura_tech/projects/vignesh_bharathi/Git_branch_study/jupyter_notebook/StatewiseTestingDetails.csv',sep=',')
+
+#    # dateWise sorting
+
+    def sort_date(q, x):
+        if q is None:
+            y = df.loc[df["Date"] == str(x)]
+        else:
+            y = df.loc[df[str(q)] == str(x)]
+
+        json_records = y.reset_index().to_json(orient = 'records')
+        data = json.loads(json_records)
+        return data
+
+    if request.method == "POST":
+        datef=request.POST.get('date')
+        statef = request.POST.get('state')
+        data = sort_date(statef, datef)
+        print(data)
+         
+    else:
+        json_records = df.reset_index().to_json(orient = 'records')
+        data = json.loads(json_records)
+    
+    
+
+    #State wise sort
+
+    # def state_sort(x):
+    #     y = df.loc[df["State"]== str(x)]
+    #     json_records = y.reset_index().to_json(orient = 'records')
+    #     data = json.loads(json_records)
+    #     return data
+
+    # if request.method == "POST":
+    #     statef = request.POST.get("state")
+    #     data = state_sort(statef)
+
+    # else:
+    #     json_records = df.reset_index().to_json(orient = 'records')
+    #     data = json.loads(json_records)
+
+
+
+
+    
+    return render(request, "download_home.html", {"df": data})
