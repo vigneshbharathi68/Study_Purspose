9f184d20702265ea03fb0757a56c950a8e70a55b
sarathi475 <sarathi475@gmail.com>
2020-12-14 11:40:38 +0530

    Few changes

--- analytics/dashboard/views.py
+++ analytics/dashboard/views.py
@@ -12,14 +12,19 @@ def dashboard_home(request):
 
     df = pd.read_csv('/home/chadura_tech/projects/vignesh_bharathi/Git_branch_study/jupyter_notebook/StatewiseTestingDetails.csv',sep=',')
 
+    json_records = df.reset_index().to_json(orient = 'records')
+    data = json.loads(json_records)
+
 #    # dateWise sorting
 
-    def sort_date(q, x):
-        if q is None:
-            y = df.loc[df["Date"] == str(x)]
-        else:
-            y = df.loc[df[str(q)] == str(x)]
+    def sort_date(x):
+        y = df.loc[df["Date"] == str(x)]
+        json_records = y.reset_index().to_json(orient = 'records')
+        data = json.loads(json_records)
+        return data
 
+    def sort_state(x):
+        y = df.loc[df["State"] == str(x)]
         json_records = y.reset_index().to_json(orient = 'records')
         data = json.loads(json_records)
         return data
@@ -27,30 +32,13 @@ def dashboard_home(request):
     if request.method == "POST":
         datef=request.POST.get('date')
         statef = request.POST.get('state')
-        data = sort_date(statef, datef)
-        print(data)
-         
-    else:
-        json_records = df.reset_index().to_json(orient = 'records')
-        data = json.loads(json_records)
-    
-    
+        
+        print(statef)
+        if datef:
+            data = sort_date(datef)
+        if statef:
+            data = sort_state(statef)
 
-    #State wise sort
-
-    # def state_sort(x):
-    #     y = df.loc[df["State"]== str(x)]
-    #     json_records = y.reset_index().to_json(orient = 'records')
-    #     data = json.loads(json_records)
-    #     return data
-
-    # if request.method == "POST":
-    #     statef = request.POST.get("state")
-    #     data = state_sort(statef)
-
-    # else:
-    #     json_records = df.reset_index().to_json(orient = 'records')
-    #     data = json.loads(json_records)
 
 
 
--- django_analytics_project/analytics_project/views.py
+++
@@ -1,4 +0,0 @@
-from django.shortcuts import render
-
-def all_practices(request):
-    return render(request, 'all_practices.html')
